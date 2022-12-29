b3092f63bfee22d81909ca604f198e4514018285
kev3334 <kev3334@gmail.com>
2022-12-29 11:13:27 -0500

    Se creo el modulo elementos y el componente dado

---
+++ src/app/elementos/elementos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DadoComponent } from './dado/dado.component';
+
+
+
+@NgModule({
+  declarations: [
+    DadoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ElementosModule { }
