86b3e94461f1d01327eef9041961913a14bdda80
kev3334 <kev3334@gmail.com>
2022-12-30 02:58:55 -0500

    Se agrega validacion del dado para que no repita un resultado anterior

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -10,12 +10,22 @@ export class DadoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
   numaleatorio=1
+  numanterior=0
   aleatorio(){
     this.numaleatorio=Math.round(Math.random()*10);
-    if(this.numaleatorio>6 || this.numaleatorio==0){
+    while(this.numaleatorio>6 || this.numaleatorio==0){
+      this.numaleatorio=Math.round(Math.random()*10);
+    }
+
+    if(this.numanterior==this.numaleatorio){
       this.aleatorio();
     }
+    this.numanterior=this.numaleatorio;
   }
+
+  
+
 }
